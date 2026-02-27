87687e3e5f1857722e448bcb6e5198f23926c682
Dawood Iqbal <dawood.iqbal184@gmail.com>
2026-02-27 17:15:37 +0500

    2nd option now working

--- ATM management system/bank_system.cpp
+++ ATM management system/bank_system.cpp
@@ -8,10 +8,11 @@ struct Account{
     int accountNumber;
     double balance;
     status accountStatus;
-
 };
 const int x = 10;
 Account accounts[x];
+
+
 void menu(){
     cout << "1) Open new account\n";
     cout << "2) Update balance of existing account\n";
@@ -20,7 +21,9 @@ void menu(){
     cout << "5) List details of all acounts\n";
     cout << "Press 0 to exit menu\n";
 }
-Account createAccount(Account accounts[]){
+
+
+int slotFinder(Account accounts[]){
     int y;
     for (int i = 0 ; i < x ; i++){
         if (accounts[i].name == ""){
@@ -28,45 +31,66 @@ Account createAccount(Account accounts[]){
             break;
         }
     }
+return y;
+}
+
+
+Account create(){
+    Account account;
     cin.ignore();
     cin.clear();
     cout << "Enter customer name\n";
-    getline(cin , accounts[y].name);
+    getline(cin , account.name);
     cout << "Assign user an account number\n";
-    cin >> accounts[y].accountNumber;
+    cin >> account.accountNumber;
     cout << "Enter the initial balance\n";
-    cin >> accounts[y].balance;
-    accounts[y].accountStatus = status::ACTIVE;
-    return accounts[y];
+    cin >> account.balance;
+    account.accountStatus = status::ACTIVE;
+return account;
 }
 
 
-Account findAccount(Account accounts[]){
+void createAccount(Account accounts[]){
+    int y = slotFinder(accounts);
+    accounts[y] = create();
+}
+
+int findAccount(Account accounts[]){
     string user;
+    int y;
     cout << "Enter user name to find account\n";
     getline(cin , user);
     for (int i = 0 ; i < x ; i++){
-        if (accounts[i].name != ""){
-            if (user == accounts[i].name) return accounts[i];
+        if (user == accounts[i].name) {
+            y = i;
+            break;
         }
     }
+return y;
 }
 
-
+string statusPrint(Account account){
+    account.accountStatus::ACTIVE;
+}
 void printAccount(Account account){
-    account = findAccount(accounts);
     cout << "Account holder name : "<< account.name<<endl;
     cout << "Account number : "<< account.accountNumber<<endl;
     cout << "Balance : "<< account.balance<<endl;
-    cout << "Account status : ";
+    cout << "Account status : "<< account.accountStatus;
     account.accountStatus = status::ACTIVE;
 }
-Account updateAccount(Account account){
-    account = findAccount(accounts);
+
+
+Account update(Account account){
+    account = accounts[findAccount(accounts)];
     printAccount(account);
     cout << "Enter the new balance of account\n";
     cin >> account.balance;
     return account;
+}
+
+void updateAccount(Account accounts[]){
+
 }
 int main (){
     int choice = 6;
