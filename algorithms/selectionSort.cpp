bf183b75fdeb662e976f2ab8e441276ed61af995
Dawood Iqbal <dawood.iqbal184@gmail.com>
2026-03-12 05:01:31 +0500

    hands-on selection sort

--- algorithms/selectionSort.cpp
+++ algorithms/selectionSort.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 using namespace std;
+
+
 void swap(int array[], int minIndex, int start){
     int temp = array[minIndex];
     array[minIndex] = array[start];
     array[start] = temp;
 }
+
+
 int minIndex(int array[], int start , int size){
     int min = start;
     for (int i = start+1 ; i < size ; i++){
@@ -35,11 +39,14 @@ void output(int array[] , int size){
 
 int main(){
     int start = 0;
-    int array[5];
-    input(array , 5);
-    for (int i = 0 ; i < 5 ; i++){
-        output(array , 5);
-        int min = minIndex(array , start , 5);
+    int size;
+    cout << "Enter the size of un-sorted array : ";
+    cin >> size;
+    int array[size];
+    input(array , size);
+    for (int i = 0 ; i < size ; i++){
+        output(array , size);
+        int min = minIndex(array , start , size);
         swap(array , min , start);
         start++;
     }
