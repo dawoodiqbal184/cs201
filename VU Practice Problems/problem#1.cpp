89f79733b535adbf40b6a3752a236a7bfcb63431
Dawood Iqbal <dawood.iqbal184@gmail.com>
2026-03-17 11:30:13 +0500

    solved the problem

--- VU Practice Problems/problem#1.cpp
+++ VU Practice Problems/problem#1.cpp
@@ -13,17 +13,30 @@ void inputSalary(int salary[], int size){
 }
 
 
+void output(int array[][2] , int rows){
+    cout <<left<<setw(12)<<"Employ"<<left<<setw(12)<<"Base salary"<<left<<setw(12)<<"Net salary\t"<<endl;
+    for (int i = 0; i < rows ; i++){
+        cout <<left<<setw(12)<<i+1;
+       for (int j = 0 ; j < 2 ; j++){
+        cout <<left<<setw(12)<< array[i][j];
+       }
+       cout << endl;
+    }
+    
+}
+
+
 int calSalary(int salary){
     float tax;
-        if (salary > 5000 && salary < 10000) {
+        if (salary > 5000 && salary <= 10000) {
             tax = salary * (0.05);
             salary -= tax;
         }
-        else if (salary > 10000 && salary < 15000) {
+        else if (salary > 10000 && salary <= 15000) {
             float tax = salary * (0.1);
             salary = salary - tax;
         }
-        else if (salary > 15000 && salary < 20000) {
+        else if (salary > 15000 && salary <= 20000) {
             float tax = salary * (0.15);
             salary = salary - tax;
         }
@@ -34,50 +47,27 @@ int calSalary(int salary){
     return salary;
 }
 
-
-int* converter(int array[] , int size){
-    int cols = 2;
-    int arrayNew[size][cols];
-    for (int i = 0 ; i < size ; i++){
-        arrayNew[i][0] = array[i];
-        arrayNew[i][1] = calSalary(arrayNew[i][0]);
-    }
-    int* ptr = *arrayNew;
-    cout <<left<<setw(10)<<"Employ"<<left<<setw(12)<<"Base salary"<<left<<setw(12)<<"Net salary\t"<<endl;
-    for (int i = 0; i < size ; i++){
-        cout <<left<<setw(10)<<i+1 <<left<<setw(12)<<*(ptr+i)<<left<<setw(12)<<*(ptr+i+size)<<endl;
-    }
-    return ptr;
-}
-
-
-void tracker(int** array , int size){
+void tracker(int array[][2] , int rows){
     
-    for(int i = 0; i < size ; i++){
-        for(int j = 0 ; j < size ; j++){
-            if (*(array + i) > *(array+j) && *(array + size + i) < *(array + size +j)){
-                cout << "Employ # "<<i<<" is unlucky having base salary more than employ # "<<j<<" but net salary is lesser\n";
+    for(int i = 0; i < rows ; i++){
+        for(int j = 0 ; j < 2 ; j++){
+            if (array[i][0] > array[j][0] && array[j][1]> array[i][1]){
+                cout << "Employ # "<<i+1<<" is unlucky having base salary more than employ # "<<j+1<<" but net salary is lesser\n";
             }
         }
     }
 }
 
 
-void output(int** ptr , int size){
-    cout <<"Employ \t"<<"Base salary\t"<<"Net salary\t"<<endl;
-    for (int i = 0; i < size ; i++){
-        cout <<i <<"\t"<<**ptr<<"\t"<<**(ptr+size)<<endl;
-        ptr++;
+void converter(int array[] , int size){
+    int arrayNew[size][2];
+    for (int i = 0 ; i < size ; i++){
+        arrayNew[i][0] = array[i];
+        arrayNew[i][1] = calSalary(arrayNew[i][0]);
     }
-    
-}
-
-
-void worker(int array[] , int *size){
-    inputSalary(array , *size);
-    int* ptr = converter(array , *size);
-    tracker(&ptr , *size);
-    output(&ptr , *size);
+    int* ptr = *arrayNew;
+    output(arrayNew , size);
+    tracker(arrayNew , size);
 }
 
 
@@ -86,9 +76,7 @@ int main (){
     cout << "Enter the number of employees\n";
     cin >> size;
     int array[size];
-    int* ptr;
     inputSalary(array , size);
-    ptr = converter(array , size);
-    tracker(&ptr , size);
+    converter(array , size);
     return 0;
 }
