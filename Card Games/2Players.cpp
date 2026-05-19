5bd771d8be4b22093ac8f98f8d493be8c24aaaa7
Dawood Iqbal <dawood.iqbal184@gmail.com>
2026-05-19 23:02:57 +0500

    Checking classes with 2 players

---
+++ Card Games/2Players.cpp
@@ -0,0 +1,17 @@
+#include "Dealer.h"
+#include "player.h"
+
+int main(){
+    Dealer tableDealer;
+    Player dawood("Dawood" , 5000);
+    Player opponent("Opponent" , 5000);
+    tableDealer.shuffle();
+    for (int i = 0 ; i < 4 ; i++){
+        dawood.receiveCards(tableDealer.deal());
+        opponent.receiveCards(tableDealer.deal());
+    }
+
+    dawood.showCards();
+    opponent.showCards();
+    return 0;
+}
