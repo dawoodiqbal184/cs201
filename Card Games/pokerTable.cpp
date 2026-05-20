e3fbdf0c5af2eaf2a1e53510cb91ef65a931cb20
Dawood Iqbal <dawood.iqbal184@gmail.com>
2026-05-20 11:27:01 +0500

    A full Texas Hold'em all table is out

--- Card Games/pokerTable.cpp
+++ Card Games/pokerTable.cpp
@@ -3,6 +3,7 @@
 #include "player.h"
 #include <iostream>
 #include <string>
+using namespace std;
 
 std::string names[4] = {"John" , "Thomas" , "Nick" , "Abraham"};
 
@@ -10,71 +11,115 @@ class texasTable{
     private:
         int players;
         int cards = 2;
+        Player array[4];
+        Dealer dealer;
+        Card community[5];
     public:
-        Dealer summonDealer();
-        Player* addPlayers();
+        void addPlayers();
+        void startGame();
+        void dealCards();
+        void dealFlop();
+        void dealTurn();
+        void dealRiver();
+        void showdown();
+        
 
         texasTable(){
-            summonDealer();
             std::cout << "How many real players are there : ";
             std::cin >> players;
+            std::cin.ignore();
             addPlayers();
         }
 };
 
 int main(){
     texasTable table;
-    Dealer tableDealer = table.summonDealer();
-    Player* array = table.addPlayers();
-    tableDealer.shuffle();
-    for (int j = 0 ; j < 2 ; j++){
-        for (int i = 1 ; i <= 4 ; i++){
-            array[i%4].receiveCards(tableDealer.deal());
-        }
-    }
-    for (int j = 0 ; j < 2 ; j++){
-        for (int i = 1 ; i <= 4 ; i++){
-            array[i%4].showCards();
-        }
-    }
+    table.startGame();
+    table.dealCards();
+    table.dealFlop();
+    table.dealTurn();
+    table.dealRiver();
+    table.showdown();
     
 
 }
 
-Dealer texasTable::summonDealer(){
-    Dealer pokerDealer;
-    return pokerDealer;
-}
-
-Player* texasTable::addPlayers(){
-    Player* ptr;
+void texasTable::addPlayers(){
     if (players > 0){
         if (players < 4){
-            ptr = new Player[4];
             for (int i = 0 ; i < players ; i++){
+                int chips;
                 std::string name;
-                std::cout << "Enter the name of player # "<<i<<" : ";
+                std::cout << "Enter the name of player # "<<i+1<<" : ";
                 getline(std::cin , name);
-                ptr[i] = Player(name );
+                cout << "Enter the amount of chips of player # "<<i+1<<" : ";
+                cin >> chips;
+                std::cin.ignore();
+                array[i] = Player(name , chips);
             }
             for (int i = players ; i < 4 ; i++){
                 std::string name;
                 name = names[rand()% 4];
-                ptr[i] = Player(name);
+                array[i] = Player(name);
             }
         }
         else{
-            ptr = new Player [players];
             for (int i = 0 ; i < players ; i++){
                 std::string name;
-                std::cin.ignore();
+                int chips;
                 std::cout << "Enter the name of player # "<<i<<" : ";
                 getline(std::cin , name);
-                ptr[i] = Player(name);
+                cout << "Enter the amount of chips of player # "<<i<<" : ";
+                cin >> chips;
+                std::cin.ignore();
+                array[i] = Player(name , chips);
             }
         } 
     }
-    else return nullptr;
-    return ptr;
+    else {
+        cout << "Number of players can't be smaller than zero!\n";
+        return;
+    }
+   
 }
 
+void texasTable::startGame(){
+    cout << "Dealer is shuffling the cards\n";
+    dealer.shuffle();
+}
+
+
+void texasTable::dealCards(){
+    cout << "Dealer is dealing cards\n";
+    for (int j = 0 ; j < cards ; j++){
+        for (int i = 0 ; i < 4 ; i++){
+            array[i%4].receiveCards(dealer.deal());
+        }
+    }
+}
+        
+
+void texasTable::dealFlop(){
+    for (int i = 0 ; i < 3 ; i++){
+        community[i] = dealer.deal();
+        cout << community[i]<< " ";
+    }
+}
+        
+
+void texasTable::dealTurn(){
+    community[3] = dealer.deal();
+    cout << community[3]<<" ";
+}
+        
+
+void texasTable::dealRiver(){
+    community[4] = dealer.deal();
+    cout << community[4]<<" \n";
+}
+
+void texasTable::showdown(){
+    for (int i = 0 ; i < 4 ; i++){
+        array[i].showCards();
+    }
+}
